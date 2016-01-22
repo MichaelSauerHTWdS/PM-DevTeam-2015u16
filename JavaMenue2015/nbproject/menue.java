7219ccbf6e7320affe1b5ea3c8ba1b739521748a
mike <mike@192.168.201.105>
2016-01-22 14:42:58 +0100

    Kommentarkopf eingefügt

--- JavaMenue2015/nbproject/menue.java
+++ JavaMenue2015/nbproject/menue.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Gerüst für ein kleines Java-Menue um mit Git VCS zu lernen
+ * Michael Sauer, 22.01.2016
+ * 
  */
 import java.util.Scanner;
 /**
