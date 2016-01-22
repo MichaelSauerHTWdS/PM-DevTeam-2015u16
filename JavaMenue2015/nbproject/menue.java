6a56895b4209baa0033255d7e342ccd78be4d501
mike <mike@192.168.201.105>
2016-01-22 14:36:45 +0100

    Menueprogramm als Grundgerüst für Projektteam zum weiterbauen

---
+++ JavaMenue2015/nbproject/menue.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author mike
+ */
+
+
+public class menue{
+        public static void main(String[] args) {
+        	final int PROGRAMMENDE=0;
+        	boolean isEnde=false;
+        	while(!isEnde){
+                	Scanner scanner = new Scanner(System.in);
+                	int wahl = scanner.nextInt();
+                	switch (wahl) {
+                        	case PROGRAMMENDE:
+                                	System.out.println("Ciao");
+                                	isEnde=true;
+                        	break;
+                        	case 1:
+                                	System.out.println("Michael Sauer");
+                        	break;
+                        	case 2:
+              //wer da sonst noch so wäre ...
+                        	break;
+                        	default:
+                                	System.out.println("Fehler: Kenne ich nicht!");
+                        	}
+                	}
+        }
+	}
