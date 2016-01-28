97464066ecc18d3f35268346d73fed6f75cc430e
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2016-01-28 14:29:29 +0100

    merge

--- JavaMenue2015/nbproject/menue.java
+++ JavaMenue2015/nbproject/menue.java
@@ -26,7 +26,6 @@ static int SuperGehimesMasterPasswort=42;
 
 
         public static void main(String[] args) {
-			
         	final int PROGRAMMENDE=0;
         	boolean isEnde=false;
         	while(!isEnde){
@@ -41,12 +40,11 @@ static int SuperGehimesMasterPasswort=42;
                         	case PROGRAMMENDE:
                                 	System.out.println("Ciao");
                                 	isEnde=true;
-                        	
+				break;
                         	case 1:
                                 	System.out.println("Michael Sauer");
 
                         	break;
-<<<<<<< HEAD
                         	case 42:
 									for(int i = 0; i<5; i++){
 										for(int j = 0; j < 5; j++)
@@ -65,15 +63,10 @@ static int SuperGehimesMasterPasswort=42;
                         	case 4:
 					System.out.println("Jan Dillenkofer");
                         	break;
-=======
-
-                        	case 2:
-                        			System.out.println("Mike Mann");
->>>>>>> a6d075b0ac737f456b2ac47e99a3034608f9f9a0
 
                         	case 666:
 				System.out.println("Steven Weis");
-							
+				break;
 
 				case 777:
 				System.out.println("I´m not a rapper, i´m a singer with a flow");
